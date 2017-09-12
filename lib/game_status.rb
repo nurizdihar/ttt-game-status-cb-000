b6795bae2f63f2174da6688a695210fc83604cbe
Nur Izdihar Binte Ismail <nurizdiharismail@gmail.com>
2017-09-12 14:06:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,3 +32,13 @@ def full?(board)
     return false
   end
 end
+
+def draw(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif !won?(board) && !full(board)
+    return false
+  elsif won?(board) 
+    return false
+  end
+end
